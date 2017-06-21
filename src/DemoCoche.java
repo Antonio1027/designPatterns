0896b3f74b64ec760a93e6b5cb12fbc6b7871426
Roberto Leroy Monroy Ruiz <roberto.monroy@enova.mx>
2017-06-21 16:16:56 -0500

    segundo ejercicio

---
+++ src/DemoCoche.java
@@ -0,0 +1,12 @@
+
+
+class DemoCoche
+{
+	public static void main(String[] args){
+
+		Coche coche = new Coche();
+                coche.modelo="mazda 3";
+                coche.color="plata";
+                System.out.println(coche.modelo+" , "+coche.color);
+	}
+}
